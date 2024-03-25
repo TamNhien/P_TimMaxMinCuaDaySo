c532edaf64857c5e848e03292e341103811ebd32
TamNhien <canhthien91@gmail.com>
2024-03-25 21:20:05 +0700

    Add project files.

---
+++ P_TimMaxMinCuaDaySo.py
@@ -0,0 +1,8 @@
+﻿numbers = []
+
+while (num := int(input("Nhập một số nguyên (nhập -1 để kết thúc) : "))) != -1:
+    numbers.append(num)
+
+print("Số lớn nhất : ", max(numbers) if numbers else "Dãy số trống!")
+print("Số nhỏ nhất : ", min(numbers) if numbers else "Dãy số trống!")
+
